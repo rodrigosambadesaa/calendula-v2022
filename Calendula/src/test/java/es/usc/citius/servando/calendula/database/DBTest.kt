de9fad4732f3799bd7152312c2e2ecbb6bb49363
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2026-09-24 21:18:52 +0200

    test: pin database Robolectric tests to supported API 27

--- Calendula/src/test/java/es/usc/citius/servando/calendula/database/DBTest.kt
+++ Calendula/src/test/java/es/usc/citius/servando/calendula/database/DBTest.kt
@@ -28,11 +28,13 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 import androidx.test.core.app.ApplicationProvider
 
 
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [27])
 class DBTest {
 
     companion object {
