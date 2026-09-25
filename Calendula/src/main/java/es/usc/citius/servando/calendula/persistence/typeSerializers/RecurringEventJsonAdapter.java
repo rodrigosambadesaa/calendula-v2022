4e4be2327a7f5015bf0195e647e649353fda2edf
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2026-09-25 19:08:20 +0200

    cleanup: route recurrence parse failures through LogUtil

--- Calendula/src/main/java/es/usc/citius/servando/calendula/persistence/typeSerializers/RecurringEventJsonAdapter.java
+++ Calendula/src/main/java/es/usc/citius/servando/calendula/persistence/typeSerializers/RecurringEventJsonAdapter.java
@@ -31,6 +31,7 @@ import org.joda.time.format.ISODateTimeFormat;
 
 import java.io.IOException;
 
+import es.usc.citius.servando.calendula.util.LogUtil;
 import es.usc.citius.servando.calendula.scheduling.model.EventInstance;
 import es.usc.citius.servando.calendula.scheduling.model.recur.DailyFixedTime;
 import es.usc.citius.servando.calendula.scheduling.model.recur.RecurringEvent;
@@ -137,7 +138,7 @@ public class RecurringEventJsonAdapter extends TypeAdapter<RecurringEvent> {
             }
             in.endObject();
         } catch (InvalidRecurrenceRuleException e) {
-            e.printStackTrace();
+            LogUtil.e(TAG, "Invalid recurrence rule while deserializing event", e);
         }
         return event;
     }
