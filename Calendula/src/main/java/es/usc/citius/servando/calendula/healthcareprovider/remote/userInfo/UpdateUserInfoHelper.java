e8b80727a3092efe9abfe141769e8fbb85579051
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2026-09-26 15:16:57 +0200

    fix: handle logout race in user-info auth interceptor

--- Calendula/src/main/java/es/usc/citius/servando/calendula/healthcareprovider/remote/userInfo/UpdateUserInfoHelper.java
+++ Calendula/src/main/java/es/usc/citius/servando/calendula/healthcareprovider/remote/userInfo/UpdateUserInfoHelper.java
@@ -21,6 +21,7 @@ package es.usc.citius.servando.calendula.healthcareprovider.remote.userInfo;
 import android.content.Context;
 import androidx.annotation.NonNull;
 
+import net.openid.appauth.AuthState;
 import net.openid.appauth.TokenResponse;
 
 import org.apache.commons.text.WordUtils;
@@ -134,9 +135,10 @@ public class UpdateUserInfoHelper {
                 .addInterceptor(new Interceptor() {
                     @Override
                     public okhttp3.Response intercept(@NonNull Chain chain) throws IOException {
-                        final String currentAccessToken = LoginStateManager.getInstance().getCurrentAuthState().getAccessToken();
+                        final AuthState state = LoginStateManager.getInstance().getCurrentAuthState();
+                        final String currentAccessToken = state != null ? state.getAccessToken() : null;
                         if (currentAccessToken == null) {
-                            throw new IllegalStateException("Access token is null!");
+                            throw new IOException("Access token is unavailable");
                         }
                         final Request request = chain.request().newBuilder()
                                 .addHeader("Authorization", String.format("%s %s", TokenResponse.TOKEN_TYPE_BEARER, currentAccessToken))
